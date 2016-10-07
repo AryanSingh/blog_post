18adafd7cda2e7caf9bc0c0d6d0bf32c8c7f3df1
aryan singh <aryan@iitgn.ac.in>
2016-10-08 04:14:24 +0530

    form validation being done

---
+++ src/components/posts_index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+	componentWillMount() {
+		this.props.fetchPosts();
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn-primary">
+						Add a Post
+					</Link>
+				</div>
+				List of blog posts
+			</div>
+		);
+	}
+}
+
+
+export default connect(null, { fetchPosts })(PostsIndex);
+

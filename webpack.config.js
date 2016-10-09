632e3f1f5f3bf8e8bb95f53aaa444eeae827ff9e
aryan singh <aryan@iitgn.ac.in>
2016-10-09 21:48:04 +0530

    testing5

--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,15 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel-loader',
+      loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
   resolve: {
-    extension: ['', '.js', '.jsx'] 
+    extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,

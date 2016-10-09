991a1751f1acfa4038d27aa9953b61b7434797b4
aryan singh <aryan@iitgn.ac.in>
2016-10-09 21:08:57 +0530

    testing5

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel-loader',
+      loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
